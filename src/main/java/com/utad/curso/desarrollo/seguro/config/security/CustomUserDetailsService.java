eb072d5471509d01b012f80011f5d04a7615eb03
josemife <josemife@LES006636.corp.capgemini.com>
2019-05-03 17:27:52 +0200

    Antes de Oauth2

--- src/main/java/com/utad/curso/desarrollo/seguro/config/security/CustomUserDetailsService.java
+++ src/main/java/com/utad/curso/desarrollo/seguro/config/security/CustomUserDetailsService.java
@@ -1,64 +1,34 @@
 package com.utad.curso.desarrollo.seguro.config.security;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Arrays;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-@Configuration // para que se guarde la clase en el contexto y al preguntar por
-				// userdetailservide le de esta
+import com.utad.curso.desarrollo.seguro.entiy.UserEntity;
+import com.utad.curso.desarrollo.seguro.repository.UsersRepository;
+
+@Configuration
 public class CustomUserDetailsService implements UserDetailsService {
 
-	private String dbUrl = "jdbc:mysql://127.0.0.1:3306/curso-desarrollo-seguro?allowMultiQueries=true&autoReconnect=true&useSSL=false&characterEncoding=UTF-8";
-
-	// private String dbUsername = " curso-desarrollo-seguro";
-
-	// private String dbPassword =
-	// "YMGPHSc93WhKhek5E3Sv6gVECZ4kP3NZa6AYbyjWpXURUHgjcG5pvYqYfSBbAAJ8YZBa6tNKcMscnXArhL2xftjqr53LqaHe7FJGzhpPa22vxSzKjcATTYtaqsm552as";
-
-	@Value("${dbusername}")
-	private String dbUsername;
-
-	@Value("${dbPassword}")
-	private String dbPassword;
-
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private UsersRepository usersRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		String query = String.format("select * from users where username = '%s';", username);
+		UserEntity user = usersRepository.findByUsername(username);
 
-		logger.info("usuarioBD:" + dbUsername);
-		logger.info("passwordBD:" + dbPassword);
-
-		logger.info("query:" + query);
-
-		try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(query)) {
-
-			if (resultSet.next()) {
-				return new org.springframework.security.core.userdetails.User(username, resultSet.getString("password"),
-						Arrays.asList(new SimpleGrantedAuthority(resultSet.getString("role"))));
-			}
-
-		} catch (SQLException e) {
+		if (user == null) {
 			throw new UsernameNotFoundException("");
 		}
 
-		throw new UsernameNotFoundException("");
+		return new org.springframework.security.core.userdetails.User(username, user.getPassword(),
+				Arrays.asList(new SimpleGrantedAuthority(user.getRole())));
 
 	}
 
--- src/main/java/com/utad/curso/desarrollo/seguro/config/security/SecurityConfig.java
+++ src/main/java/com/utad/curso/desarrollo/seguro/config/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.utad.curso.desarrollo.seguro.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -20,6 +22,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder; // será CustomPasswordEncoder
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
