9e01680555458d41011e43df88957ed218dc8bb5
josemife <josemife@LES006636.corp.capgemini.com>
2019-05-03 17:40:33 +0200

    2

--- src/main/java/com/utad/curso/desarrollo/seguro/config/security/SecurityConfig.java
+++ src/main/java/com/utad/curso/desarrollo/seguro/config/security/SecurityConfig.java
@@ -1,60 +1,38 @@
 package com.utad.curso.desarrollo.seguro.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig
+        extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailsService; // será la clase CustomUserDetailServices
+    @Autowired
+    private OpenIdConnectFilter openIdConnectFilter;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder; // será CustomPasswordEncoder
+    @Override
+    protected void configure(
+            HttpSecurity http)
+            throws Exception {
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+        // OAuth2.
+        http
+                .addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+                .addFilterAfter(openIdConnectFilter, OAuth2ClientContextFilter.class)
+                .httpBasic().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/oauth2-callback"));
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+        // Authorization.
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
 
-		// Login and Logout.
-		http.formLogin().loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/")
-				.failureUrl("/login?failure=true").and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
-
-		// Authentication Provider.
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService); // como hacer para probar las credenciales
-		authProvider.setPasswordEncoder(passwordEncoder);
-		http.authenticationProvider(authProvider);
-
-		// Authorization.
-		http.authorizeRequests()
-
-				// Allow Base URLs
-				.and().authorizeRequests().antMatchers("/login").permitAll() // se permite a todo el mundo
-				.and().authorizeRequests().antMatchers("/logout").permitAll()// se permite a todo el mundo
-
-				// Require Authentication for any other URL
-				.and().authorizeRequests().anyRequest().fullyAuthenticated(); // para acceder al resto se debe estar
-																				// autenticado
-
-		// CSRF.
-		http.csrf().disable();
-
-	}
+    }
 
 }
